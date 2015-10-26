76dbba0a4ce9b583c77a5743de61db0c6136ae25
kolomiytsev <leonid.kolomiytsev@gmail.com>
2015-10-26 15:56:47 +0200

    Graceful server shutdown added

--- server.js
+++ server.js
@@ -38,3 +38,23 @@ mongo.connect(dbUrl, function() {
 
     });
 });
+
+var gracefulShutdown = function() {
+    logger.info("Received kill signal, shutting down gracefully.");
+    server.close(function() {
+        logger.info("Closed out remaining connections.");
+        process.exit()
+    });
+
+    // if after
+    setTimeout(function() {
+        logger.info("Could not close connections in time, forcefully shutting down");
+        process.exit()
+    }, 10*1000);
+};
+
+// listen for TERM signal .e.g. kill
+process.on ('SIGTERM', gracefulShutdown);
+
+// listen for INT signal e.g. Ctrl-C
+process.on ('SIGINT', gracefulShutdown);
